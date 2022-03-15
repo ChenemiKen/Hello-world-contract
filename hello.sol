5e3d79f02b61753b06c9470bc74167bcd7413593
"ChenemiKen" <"chenemiken15@gmail.com">
2022-03-15 15:04:26 +0100

    initial commit

---
+++ hello.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.8.10;
+
+contract Hello{
+    string public greeting = "Hello world!";
+
+    function setGreeting(string memory _greeting) public{
+        greeting = _greeting;
+    }
+
+    function greet() public view returns(string memory){
+        return greeting;
+    }
+}
